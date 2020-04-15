549832dc6449dbc7d53def3f6c08097fd51f76ea
Ben Carter <benjohn.carter@gmail.com>
2020-04-15 19:40:35 +0100

    Fixed yarn build

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.sass';
-
-function Example() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-        </button>
-    </div>
-  );
-}
 
 function hero() {
   return (
-    <section class="hero is-primary">
-      <div class="hero-body">
-        <div class="container">
-          <h1 class="title">
+    <section className="hero is-primary">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">
             Primary title
           </h1>
-          <h2 class="subtitle">
+          <h2 className="subtitle">
             Primary subtitle
           </h2>
         </div>
@@ -33,61 +20,72 @@ function hero() {
 
 function navBar() {
   return (
-    <nav class="navbar" role="navigation" aria-label="main navigation">
-      <div class="navbar-brand">
-        <a class="navbar-item" href="https://bulma.io">
-          <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <a className="navbar-item" href="https://bulma.io">
+          <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" alt="" />
         </a>
 
-        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <a
+          href="#/"
+          role="button"
+          className="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
       </div>
 
-      <div id="navbarBasicExample" class="navbar-menu">
-        <div class="navbar-start">
-          <a class="navbar-item">
+      <div id="navbarBasicExample" className="navbar-menu">
+        <div className="navbar-start">
+          <a className="navbar-item" href="#Home">
             Home
-      </a>
+          </a>
 
-          <a class="navbar-item">
+          <a className="navbar-item" href="#Documentation">
             Documentation
-      </a>
+          </a>
 
-          <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link">
+          <div className="navbar-item has-dropdown is-hoverable">
+            <a href="#more" className="navbar-link">
               More
-        </a>
+            </a>
 
-            <div class="navbar-dropdown">
-              <a class="navbar-item">
+            <div className="navbar-dropdown">
+              <a className="navbar-item"
+                href="#About">
                 About
-          </a>
-              <a class="navbar-item">
+              </a>
+              <a className="navbar-item"
+                href="#Jobs">
                 Jobs
-          </a>
-              <a class="navbar-item">
+              </a>
+              <a className="navbar-item"
+                href="#Contact">
                 Contact
-          </a>
-              <hr class="navbar-divider" />
-              <a class="navbar-item">
+              </a>
+              <hr className="navbar-divider" />
+              <a className="navbar-item"
+                href="#Report">
                 Report an issue
-          </a>
+              </a>
             </div>
           </div>
         </div>
 
-        <div class="navbar-end">
-          <div class="navbar-item">
-            <div class="buttons">
-              <a class="button is-primary">
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="buttons">
+              <a href="#signup" className="button is-primary">
                 <strong>Sign up</strong>
               </a>
-              <a class="button is-light">
+              <a href="#login" className="button is-light">
                 Log in
-          </a>
+              </a>
             </div>
           </div>
         </div>
@@ -98,38 +96,32 @@ function navBar() {
 
 function card() {
   return (
-    <div class="card">
-      <header class="card-header">
-        <p class="card-header-title">
+    <div className="card">
+      <header className="card-header">
+        <p className="card-header-title">
           Component
-    </p>
-        <a href="#" class="card-header-icon" aria-label="more options">
-          <span class="icon">
-            <i class="fas fa-angle-down" aria-hidden="true"></i>
+        </p>
+        <a href="#more-options" className="card-header-icon" aria-label="more options">
+          <span className="icon">
+            <i className="fa fa-address-book" aria-hidden="true"></i>
           </span>
         </a>
       </header>
-      <div class="card-content">
-        <div class="content">
+      <div className="card-content">
+        <div className="content">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-      <a href="#">@bulmaio</a>. <a href="#">#css</a> <a href="#">#responsive</a>
           <br />
-          <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+          <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
         </div>
       </div>
-      <footer class="card-footer">
-        <a href="#" class="card-footer-item">Save</a>
-        <a href="#" class="card-footer-item">Edit</a>
-        <a href="#" class="card-footer-item">Delete</a>
-      </footer>
     </div>
   )
 }
 
 function footer() {
   return (
-    <footer class="footer">
-      <div class="content has-text-centered">
+    <footer className="footer">
+      <div className="content has-text-centered">
         <p>
           <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>.
           The source code is licensed <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
@@ -144,10 +136,11 @@ function footer() {
 function showTiles() {
   return (
     <div>
-      {/* {navBar()} */}
-      <section class="section">
-        <div class="container">
-
+      {navBar()}
+      <section className="section">
+        <div className="container">
+          {hero()}
+          <br />
           {card()}
         </div>
       </section>
