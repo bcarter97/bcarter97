8269704c93cb0716f7e7278f3844d68ae294e471
Ben Carter <benjohn.carter@gmail.com>
2020-04-16 16:09:13 +0100

    Added logo, fixed a refs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.sass';
+import bulmerLogo from './images/made-with-bulma-badges/made-with-bulma.png';
+import banner from './images/banner.png';
 
 function hero() {
   return (
@@ -18,79 +20,86 @@ function hero() {
   )
 }
 
-function navBar() {
+function NavBar() {
+
+  const [menuVisible, setMenuVisible] = useState(false);
+
   return (
-    <nav className="navbar" role="navigation" aria-label="main navigation">
-      <div className="navbar-brand">
-        <a className="navbar-item" href="https://bulma.io">
-          <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" alt="" />
-        </a>
 
-        <a
-          href="#/"
-          role="button"
-          className="navbar-burger burger"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbarBasicExample"
-        >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
+    <nav className="navbar has-shadow is-spaced" role="navigation" aria-label="main navigation">
+      <div className="container">
+        <div className="navbar-brand">
+          <div className="navbar-item">
+            <img src={banner} height="28" alt="Site logo" />
+          </div>
 
-      <div id="navbarBasicExample" className="navbar-menu">
-        <div className="navbar-start">
-          <a className="navbar-item" href="#Home">
-            Home
-          </a>
-
-          <a className="navbar-item" href="#Documentation">
-            Documentation
-          </a>
-
-          <div className="navbar-item has-dropdown is-hoverable">
-            <a href="#more" className="navbar-link">
-              More
-            </a>
-
-            <div className="navbar-dropdown">
-              <a className="navbar-item"
-                href="#About">
-                About
-              </a>
-              <a className="navbar-item"
-                href="#Jobs">
-                Jobs
-              </a>
-              <a className="navbar-item"
-                href="#Contact">
-                Contact
-              </a>
-              <hr className="navbar-divider" />
-              <a className="navbar-item"
-                href="#Report">
-                Report an issue
-              </a>
-            </div>
+          <div
+            role="button"
+            className={`navbar-burger burger ${menuVisible ? 'is-active' : ''}`}
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+            onClick={() => setMenuVisible(!menuVisible)}
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
           </div>
         </div>
 
-        <div className="navbar-end">
-          <div className="navbar-item">
-            <div className="buttons">
-              <a href="#signup" className="button is-primary">
-                <strong>Sign up</strong>
+        <div id="navbarBasicExample" className={`navbar-menu ${menuVisible ? 'is-active' : ''}`}>
+          <div className="navbar-start">
+            <a className="navbar-item" href="#Home">
+              Home
+          </a>
+
+            <a className="navbar-item" href="#Documentation">
+              Documentation
+          </a>
+
+            <div className="navbar-item has-dropdown is-hoverable">
+              <a href="#more" className="navbar-link">
+                More
+            </a>
+
+              <div className="navbar-dropdown">
+                <a className="navbar-item"
+                  href="#About">
+                  About
               </a>
-              <a href="#login" className="button is-light">
-                Log in
+                <a className="navbar-item"
+                  href="#Jobs">
+                  Jobs
               </a>
+                <a className="navbar-item"
+                  href="#Contact">
+                  Contact
+              </a>
+                <hr className="navbar-divider" />
+                <a className="navbar-item"
+                  href="#Report">
+                  Report an issue
+              </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <div className="buttons">
+                <a href="#signup" className="button is-primary">
+                  <strong>Sign up</strong>
+                </a>
+                <a href="#login" className="button is-light">
+                  Log in
+              </a>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </nav>
+
   )
 }
 
@@ -118,35 +127,85 @@ function card() {
   )
 }
 
+function Tiles() {
+  return (
+    <div class="tile is-ancestor">
+      <div class="tile is-vertical is-8">
+        <div class="tile">
+          <div class="tile is-parent is-vertical">
+            <article class="tile is-child notification is-primary">
+              <p class="title">Vertical...</p>
+              <p class="subtitle">Top tile</p>
+            </article>
+            <article class="tile is-child notification is-warning">
+              <p class="title">...tiles</p>
+              <p class="subtitle">Bottom tile</p>
+            </article>
+          </div>
+          <div class="tile is-parent">
+            <article class="tile is-child notification is-info">
+              <p class="title">Middle tile</p>
+              <p class="subtitle">With an image</p>
+              <figure class="image is-4by3">
+                <img src="https://bulma.io/images/placeholders/640x480.png" alt="Bulma logo" />
+              </figure>
+            </article>
+          </div>
+        </div>
+        <div class="tile is-parent">
+          <article class="tile is-child notification is-danger">
+            <p class="title">Wide tile</p>
+            <p class="subtitle">Aligned with the right tile</p>
+            <div class="content">
+              Content
+        </div>
+          </article>
+        </div>
+      </div>
+      <div class="tile is-parent">
+        <article class="tile is-child notification is-success">
+          <div class="content">
+            <p class="title">Tall tile</p>
+            <p class="subtitle">With even more content</p>
+            <div class="content">
+              Content
+        </div>
+          </div>
+        </article>
+      </div>
+    </div>
+  )
+}
+
 function footer() {
   return (
     <footer className="footer">
       <div className="content has-text-centered">
-        <p>
-          <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>.
-          The source code is licensed <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
-          is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
-        </p>
+        <a href="https://bulma.io">
+          <img src={bulmerLogo} alt="Made with Bulma" width="128" height="24"></img>
+        </a>
       </div>
     </footer>
   )
 }
 
 
-function showTiles() {
+function App() {
   return (
-    <div>
-      {navBar()}
+    <body className="layout-default">
+      {NavBar()}
       <section className="section">
         <div className="container">
           {hero()}
           <br />
           {card()}
+          <br />
+          {Tiles()}
         </div>
       </section>
       {footer()}
-    </div>
+    </body>
   )
 }
 
-export default showTiles;
+export default App;
