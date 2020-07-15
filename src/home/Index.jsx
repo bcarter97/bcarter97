9c897f2a2628bdbffe2eb023e1a5a528f14e4d9d
Ben Carter <benjohn.carter@gmail.com>
2020-07-15 21:45:39 +0100

    Added about page

--- src/home/Index.jsx
+++ src/home/Index.jsx
@@ -1,123 +1,80 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Markdown from 'react-markdown';
 import { Body } from '../components';
+import pp from '../images/about/pp.jpg';
+import about from '../markdown/about.md';
 
-function Hero() {
+const SocialButtons = () => {
   return (
-    <section className="hero is-primary">
-      <div className="hero-body">
-        <div className="container">
-          <h1 className="title">Primary title</h1>
-          <h2 className="subtitle">Primary subtitle</h2>
-        </div>
-      </div>
-    </section>
-  );
-}
-
-function Card() {
-  return (
-    <div className="card">
-      <header className="card-header">
-        <p className="card-header-title">Component</p>
-        <a
-          href="#more-options"
-          className="card-header-icon"
-          aria-label="more options"
-        >
-          <span className="icon">
-            <i className="fa fa-address-book" aria-hidden="true"></i>
-          </span>
-        </a>
-      </header>
-      <div className="card-content">
-        <div className="content">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
-          iaculis mauris.
-          <br />
-          <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-        </div>
-      </div>
+    <div className="field is-grouped-centered is-grouped is-spaced">
+      <a
+        className="navbar-item has-text-black"
+        href="https://github.com/bcarter97"
+      >
+        <span className="icon">
+          <i className="fa fa-github fa-lg" aria-hidden="true" />
+        </span>
+      </a>
+      <a
+        className="navbar-item has-text-info"
+        href="https://www.linkedin.com/in/bcarter97/"
+      >
+        <span className="icon">
+          <i className="fa fa-linkedin fa-lg" aria-hidden="true" />
+        </span>
+      </a>
     </div>
   );
-}
+};
 
-function Tiles() {
+const ProfilePicture = () => {
   return (
-    <div className="tile is-ancestor">
-      <div className="tile is-vertical is-8">
-        <div className="tile">
-          <div className="tile is-parent is-vertical">
-            <article className="tile is-child notification is-primary">
-              <p className="title">Vertical...</p>
-              <p className="subtitle">Top tile</p>
-            </article>
-            <article className="tile is-child notification is-warning">
-              <p className="title">...tiles</p>
-              <p className="subtitle">Bottom tile</p>
-            </article>
-          </div>
-          <div className="tile is-parent">
-            <article className="tile is-child notification is-info">
-              <p className="title">Middle tile</p>
-              <p className="subtitle">With an image</p>
-              <figure className="image is-4by3">
-                <img
-                  src="https://bulma.io/images/placeholders/640x480.png"
-                  alt="Bulma logo"
-                />
-              </figure>
-            </article>
-          </div>
-        </div>
-        <div className="tile is-parent">
-          <article className="tile is-child notification is-danger">
-            <p className="title">Wide tile</p>
-            <p className="subtitle">Aligned with the right tile</p>
-            <div className="content">Content</div>
-          </article>
-        </div>
-      </div>
-      <div className="tile is-parent">
-        <article className="tile is-child notification is-success">
-          <div className="content">
-            <p className="title">Tall tile</p>
-            <p className="subtitle">With even more content</p>
-            <div className="content">Content</div>
-          </div>
-        </article>
-      </div>
-    </div>
+    <figure class="image container is-128x128">
+      <img className="is-rounded" src={pp} alt="Profile picture" />
+    </figure>
   );
-}
+};
 
-const TestFunction = () => {
-  console.log('This is a test');
-  return 'this is a test';
+const Details = () => {
+  const [aboutInfo, setAboutInfo] = useState('');
+  useEffect(() => {
+    fetch(about)
+      .then((res) => res.text())
+      .then((text) => setAboutInfo(text));
+  }, []);
+
+  return <Markdown source={aboutInfo} />;
 };
 
 const About = () => {
   return (
-    <section class="hero">
-      <div class="hero-body">
-        <div class="container">
-          <h1 class="title">Hero title</h1>
-          <h2 class="subtitle">Hero subtitle</h2>
+    <div class="section">
+      <div class="container">
+        <div class="columns">
+          <div class="column is-6 is-offset-3">
+            <div class="card">
+              <div class="card-content">
+                <ProfilePicture />
+                <p className="title has-text-centered">Ben Carter</p>
+                <div class="content">
+                  <Details />
+                </div>
+              </div>
+              <div className="card-footer">
+                <p className="card-footer-item">
+                  <SocialButtons />
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
 function Home() {
-  return Body(
-    <>
-      <Hero />
-      <br />
-      <Card />
-      <br />
-      <Tiles />
-    </>
-  );
+  return <About />;
 }
 
 export { Home };
---
+++ src/markdown/about.md
@@ -0,0 +1,7 @@
+---
+## Academic
+
+| University of Sussex          |         | Grade |
+|-------------------------------|---------|-------|
+| Computer Science BSc          | 2016-19 | 2.1   |
+| Advanced Computer Science MSc | 2019-20 | 1     |
