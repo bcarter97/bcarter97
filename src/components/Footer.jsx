53ff2eb5656c6d24f756759a487e663fa6cb100f
Ben Carter <benjohn.carter@gmail.com>
2020-07-23 23:23:25 +0100

    refactor footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import bulmerLogo from '../images/made-with-bulma-badges/made-with-bulma.png';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 function Footer() {
   return (
-    <footer className="footer has-text-centered">
-      <NavLink exact to="/">
-        Home
-      </NavLink>{' '}
-      <a href="https://github.com/bcarter97">GitHub</a>
-    </footer>
+    <div className="footer has-text-centered">
+      <div className="buttons ">
+        <NavLink exact to="/" className="button is-primary">
+          Home
+        </NavLink>
+        <a href="https://github.com/bcarter97" className="button is-primary">
+          <div>GitHub</div>
+        </a>
+      </div>
+    </div>
   );
 }
 
