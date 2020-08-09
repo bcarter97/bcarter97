49680d0911accd3aaa5f2fa804156622f2766db4
Ben Carter <benjohn.carter@gmail.com>
2020-08-09 12:16:32 +0100

    banner width to stop content jumping

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -23,7 +23,7 @@ const NavBrand = ({ menuVisible, onClick }) => {
   return (
     <div className="navbar-brand">
       <NavLink exact to="/" className="navbar-item">
-        <img src={banner} height="28" alt="Site logo" />
+        <img src={banner} height="28" width="135" alt="Site logo" />
       </NavLink>
       <GitHubButton classOverride="is-hidden-desktop" />
       <LinkedInButton classOverride="is-hidden-desktop" />
