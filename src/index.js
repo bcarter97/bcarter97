98723d5bc3cd257f56be6aa3c48e1682eba503df
Ben Carter <benjohn.carter@gmail.com>
2020-10-31 19:31:28 +0000

    removed sw

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { Router } from "react-router-dom"
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { rootReducer } from './reducers/rootReducer';
-import { AuthContextProvider } from './auth/Auth';
+import React from "react";
+import { render } from "react-dom";
+import { Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { rootReducer } from "./reducers/rootReducer";
+import { AuthContextProvider } from "./auth/Auth";
 
-import './styles/styles.scss';
-import '@fortawesome/fontawesome-free/css/all.css'
-import App from './app/Index';
-import { history } from "./helpers/history"
-import * as serviceWorker from './serviceWorker';
+import "./styles/styles.scss";
+import "@fortawesome/fontawesome-free/css/all.css";
+import App from "./app/Index";
+import { history } from "./helpers/history";
+import * as serviceWorker from "./serviceWorker";
 
 const store = createStore(rootReducer);
-const apiURL = 'https://carter.gg/.netlify/identity';
+const apiURL = "https://carter.gg/.netlify/identity";
 
 render(
   <Router history={history}>
@@ -22,9 +22,8 @@ render(
         <App />
       </Provider>
     </AuthContextProvider>
-  </Router>
-  ,
-  document.getElementById('root')
+  </Router>,
+  document.getElementById("root")
 );
 
-serviceWorker.register();
+serviceWorker.unregister();
