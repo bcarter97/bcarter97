1c35042c4f287c097eab9b4541d50c8d42cdbcfd
Ben Carter <benjohn.carter@gmail.com>
2020-12-20 17:19:44 +0000

    removed page transitions

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,22 @@
-import { useLayoutEffect, useState } from "react";
 import { Route, Router } from "react-router-dom";
-import { CSSTransition } from "react-transition-group";
 
 import Nav from "./components/Nav";
-import { Contact, Home, Profile } from "./containers";
+import { Contact, Home } from "./containers";
 import { history } from "./util/history";
-
-const useWindowSize = () => {
-  const [height, setHeight] = useState(undefined);
-  useLayoutEffect(() => {
-    const updateHeight = () => {
-      setHeight(document.documentElement?.clientHeight || window.innerHeight);
-    };
-    window.addEventListener("resize", updateHeight);
-    updateHeight();
-    return () => window.removeEventListener("resize", updateHeight);
-  }, []);
-  return height;
-};
 
 const routes = [
   { path: "/", name: "Home", exact: true, Component: Home },
   { path: "/contact", name: "Contact", exact: true, Component: Contact },
-  { path: "/profile", name: "Profile", exact: false, Component: Profile },
 ];
 
 const App = () => {
-  const height = useWindowSize();
-
   return (
-    <div className="layout-default" style={{ minHeight: height }}>
+    <div className="layout-default">
       <Router history={history}>
         <Nav />
         <div className="container">
           {routes.map(({ path, exact, Component }) => (
-            <Route key={path} exact={exact} path={path}>
-              {({ match }) => (
-                <CSSTransition
-                  in={match != null}
-                  timeout={300}
-                  classNames="page"
-                  unmountOnExit
-                >
-                  <div className="page">
-                    <Component />
-                  </div>
-                </CSSTransition>
-              )}
-            </Route>
+            <Route key={path} exact={exact} path={path} component={Component} />
           ))}
         </div>
       </Router>
