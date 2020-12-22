45ffcdad9b94fedd06d774cafae0f69d4c168b79
Ben Carter <benjohn.carter@gmail.com>
2020-12-22 21:35:22 +0000

    added data-test-ids to elements

--- src/components/AboutInfo.jsx
+++ src/components/AboutInfo.jsx
@@ -1,13 +1,17 @@
 const AboutInfo = ({ message }) => {
   return (
     <div className="columns info-columns is-multiline is-vcentered is-centered is-gapless">
-      <div className="column is-12">
+      <div className="column is-12" data-test-id="about-info-column">
         <p className="is-size-5-touch is-size-4-desktop has-text-weight-bold">
           Associate Software Developer at{" "}
           <a className="social-link" href="https://www.sky.com/">
             Sky
           </a>
-          . <span className="about-message">{message}</span>.
+          .{" "}
+          <span className="about-message" data-test-id="about-message">
+            {message}
+          </span>
+          .
         </p>
       </div>
     </div>
