8c988358b15d264cca4694bda6c937e06a3f8b1a
Ben Carter <benjohn.carter@gmail.com>
2020-12-22 20:50:48 +0000

    SEO template for react-helmet

---
+++ src/components/Seo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+const Seo = ({ title }) => {
+  return (
+    <Helmet defaultTitle="Ben Carter | Home" titleTemplate="Ben Carter | %s">
+      <title>{title}</title>
+    </Helmet>
+  );
+};
+
+export default Seo;
