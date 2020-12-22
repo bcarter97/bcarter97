137baca78c5e44744e5894a4978c7521d023b127
Ben Carter <benjohn.carter@gmail.com>
2020-12-22 21:08:22 +0000

    updated app imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+import App from "./app/App";
 
 import "./styles.scss";
 import "@fortawesome/fontawesome-free/css/all.css";
