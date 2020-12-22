2b30ca2b658deea61a8b632186399f563c889e06
Ben Carter <benjohn.carter@gmail.com>
2020-12-22 19:57:00 +0000

    added low res blurred image on initial load

--- src/media/ProfilePicture.jsx
+++ src/media/ProfilePicture.jsx
@@ -1,10 +1,22 @@
-import profilePic from "../images/about/profile.webp";
+import profilePic from "../images/about/profile.png";
+import profilePicLow from "../images/about/profileLow.png";
+import useProgressiveImg from "../util/imageHook";
 
 const ProfilePicture = () => {
+  const [src, { blur }] = useProgressiveImg(profilePicLow, profilePic);
+
   return (
     <div className="image-container">
       <figure className="image">
-        <img className="image-module" src={profilePic} alt="Headshot" />
+        <img
+          className="image-module"
+          src={src}
+          alt="Headshot"
+          style={{
+            filter: blur ? "blur(10px)" : "none",
+            transition: blur ? "none" : "filter 0.1s ease-out",
+          }}
+        />
       </figure>
     </div>
   );
