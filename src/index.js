3a4c7f56f2080b26ef89fe33e1f891f8065ed462
Ben Carter <benjohn.carter@gmail.com>
2020-12-26 18:14:42 +0000

    remove bad sws

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./app/App";
+import { unregister } from "./serviceWorker";
 
 import "./styles.scss";
 import "@fortawesome/fontawesome-free/css/all.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+if (process.env.NODE_ENV === "production") {
+  unregister();
+}
