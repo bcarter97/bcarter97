72ea5a27b93a4d8c648b9b8fffb4a0df8ae50590
Ben Carter <benjohn.carter@gmail.com>
2020-12-26 18:23:10 +0000

    noop serviceworker

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,3 +1,11 @@
+// A simple, no-op service worker that takes immediate control.
+window.addEventListener("install", () => {
+  // Skip over the "waiting" lifecycle state, to ensure that our
+  // new service worker is activated immediately, even if there's
+  // another tab open controlled by our older service worker code.
+  window.skipWaiting();
+});
+
 export function unregister() {
   console.log("attempting to remove this sites broken service worker");
   if ("serviceWorker" in navigator) {
