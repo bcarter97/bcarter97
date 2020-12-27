7db368ccacbc0bb57ccb7d004e03ceb37156e3af
Ben Carter <benjohn.carter@gmail.com>
2020-12-27 00:16:41 +0000

    added back default sw with unregister

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./app/App";
-import * as serviceWorker from "./service-worker";
+import { unregister } from "./serviceWorker";
 
 import "./styles.scss";
 import "@fortawesome/fontawesome-free/css/all.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-serviceWorker.unregister();
+unregister();
--- src/service-worker.js
+++
@@ -1,7 +0,0 @@
-//eslint-disable-next-line
-self.globalThis.registration.unregister();
-
-export const unregister = () => {
-  //eslint-disable-next-line
-  self.globalThis.registration.unregister();
-};
