50f9e48cc3baf2627cc358742488db36114e41d9
Ben Carter <benjohn.carter@gmail.com>
2020-12-27 17:11:31 +0000

    removed sw references

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./app/App";
-import { register } from "./serviceWorker";
 
 import "./styles.scss";
 import "@fortawesome/fontawesome-free/css/all.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-register();
