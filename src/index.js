ed9bd0b765a6ede04c1807f5bbaee4b885013781
Ben Carter <benjohn.carter@gmail.com>
2020-12-27 01:17:09 +0000

    removed sw

--- src/index.js
+++ src/index.js
@@ -6,8 +6,4 @@ import App from "./app/App";
 import "./styles.scss";
 import "@fortawesome/fontawesome-free/css/all.css";
 
-import { register } from "./serviceWorker";
-
 ReactDOM.render(<App />, document.getElementById("root"));
-
-register();
