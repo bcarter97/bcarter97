65293e9a0df42a9aadfcf17d66dadd0430e6d4bb
Ben Carter <benjohn.carter@gmail.com>
2020-12-27 15:55:42 +0000

    registered sw

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./app/App";
+import { register } from "./serviceWorker";
 
 import "./styles.scss";
 import "@fortawesome/fontawesome-free/css/all.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+register();
