7e71b218017c72a0f458538ce405e15d6820e942
Ben Carter <benjohn.carter@gmail.com>
2021-01-02 20:47:17 +0000

    added mobile banner icon and coffee icon

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,13 @@
 import { NavLink } from "react-router-dom";
 
 import banner from "../images/banner/bannerSmall.png";
+import logo from "../images/banner/logoSmall.png";
+
+const KOFI_ATTRIBS = {
+  href: "https://ko-fi.com/L4L4345QX",
+  target: "_blank",
+  rel: "noopener noreferrer",
+};
 
 const Icon = ({ icon }) => {
   return (
@@ -16,6 +23,10 @@ const HomeIcon = () => {
 
 const ContactIcon = () => {
   return <Icon icon="far fa-paper-plane" />;
+};
+
+const KofiIcon = () => {
+  return <Icon icon="fas fa-coffee" />;
 };
 
 const NavIcon = ({ to, icon, end }) => {
@@ -34,6 +45,40 @@ const NavIcon = ({ to, icon, end }) => {
   );
 };
 
+const Banner = () => {
+  return (
+    <NavLink exact to="/" className="navbar-item">
+      <img
+        src={banner}
+        height="28"
+        width="135"
+        alt="Site logo"
+        className="is-hidden-touch"
+      />
+      <img
+        src={logo}
+        height="28"
+        width="28"
+        alt="Site logo mobile"
+        className="is-hidden-desktop"
+      />
+    </NavLink>
+  );
+};
+
+const KofiItem = () => {
+  return (
+    <a
+      className="navbar-item is-hidden-desktop kofi-item-mobile"
+      {...KOFI_ATTRIBS}
+    >
+      <span className="icon is-medium">
+        <i className="fas fa-coffee fa-lg"></i>
+      </span>
+    </a>
+  );
+};
+
 const Nav = () => {
   return (
     <nav
@@ -45,15 +90,16 @@ const Nav = () => {
     >
       <div className="container">
         <div className="navbar-brand">
-          <NavLink exact to="/" className="navbar-item">
-            <img src={banner} height="28" width="135" alt="Site logo" />
-          </NavLink>
-
+          <Banner />
+          <KofiItem />
           <NavIcon to="/" icon="fas fa-home" end />
           <NavIcon to="/contact" icon="far fa-paper-plane" />
         </div>
         <div id="main-navigation" className="navbar-menu">
           <div className="navbar-end">
+            <a {...KOFI_ATTRIBS} className="navbar-item navbar-icon kofi-item">
+              <KofiIcon /> <span className="navbar-icon">Buy me a coffee</span>
+            </a>
             <NavLink className="navbar-item navbar-icon" exact to="/">
               <HomeIcon /> <span className="navbar-icon">Home</span>
             </NavLink>
