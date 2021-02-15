27c81aa66e92de89463d2e76b3794d432712f3a1
Ben Carter <benjohn.carter@gmail.com>
2021-02-15 14:42:01 +0000

    feat: :lipstick: added particle backgrounds

---
+++ src/components/background/Background.jsx
@@ -0,0 +1,81 @@
+import Particles from "react-tsparticles";
+
+const Background = () => {
+  return (
+    <Particles
+      id="tsparticles"
+      options={{
+        backgroundMode: {
+          enable: true,
+          zIndex: 0,
+        },
+        background: {
+          color: {
+            value: "#FCFCFC",
+          },
+        },
+        fpsLimit: 60,
+        interactivity: {
+          detectsOn: "window",
+          events: {
+            onHover: {
+              enable: true,
+              mode: "grab",
+            },
+            resize: true,
+          },
+        },
+        particles: {
+          color: {
+            value: ["#000000", "#ffffff"],
+          },
+          links: {
+            color: "#000000",
+            distance: 100,
+            enable: true,
+            opacity: 0.1,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: true,
+            speed: 0.5,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              value_area: 2000,
+            },
+            value: 500,
+          },
+          opacity: {
+            value: 0.1,
+          },
+          shape: {
+            type: "edge",
+            stroke: {
+              width: 1,
+              color: "#000000",
+            },
+            polygon: {
+              nb_sides: 5,
+            },
+          },
+          size: {
+            random: true,
+            value: 2,
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+  );
+};
+
+export default Background;
