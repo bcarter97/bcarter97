790ef2a6e0f2fce030e3ec3da69df313b922dd59
Ben Carter <benjohn.carter@gmail.com>
2021-02-15 14:41:40 +0000

    refactor: :recycle: added classname for animations

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -4,7 +4,7 @@ export const LayoutDefault = ({ title, children }) => {
   return (
     <>
       <Seo title={title} />
-      <section className="section">
+      <section className="section default-section">
         <div className="columns is-multiline is-vcentered is-centered">
           <div className="column is-half">
             <div className="box">{children}</div>
