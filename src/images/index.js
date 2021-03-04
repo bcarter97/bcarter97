1defa51990013b115938e05bb961b5b8926ab9db
Ben Carter <benjohn.carter@gmail.com>
2021-03-04 23:30:19 +0000

    feat: :zap: converted images to webp

---
+++ src/images/index.js
@@ -0,0 +1,4 @@
+export { default as profileHigh } from "./about/profileHigh.webp";
+export { default as profileLow } from "./about/profileLow.webp";
+export { default as bannerSmall } from "./banner/bannerSmall.webp";
+export { default as logoSmall } from "./banner/logoSmall.webp";
