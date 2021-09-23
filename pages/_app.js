6522a386e3b64e160ae21b9fe82d72a682008d15
Ben Carter <benjohn.carter@gmail.com>
2021-09-23 21:38:41 +0100

    feat: add background svg

--- pages/_app.js
+++ pages/_app.js
@@ -2,19 +2,17 @@ import "styles/globals.scss";
 import "@fortawesome/fontawesome-free/css/all.css";
 
 import Navbar from "components/Navbar";
-import Background from "components/Particles";
 import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <div className="layout-default">
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Background />
       <Navbar />
       <Component {...pageProps} />
-    </>
+    </div>
   );
 }
 
