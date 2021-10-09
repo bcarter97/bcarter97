a069305b3ae5a9f2bfd5ee90b1f8ab8c8ab77d45
Ben Carter <benjohn.carter@gmail.com>
2021-10-09 01:55:30 +0100

    chore: add default exports for folders

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Button } from "./Button";
+export { default as Column } from "./Column";
+export { default as Layout } from "./Layout";
+export { default as Navbar } from "./Navbar";
+export { default as NavLink } from "./NavLink";
