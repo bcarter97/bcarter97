f6832e32e61260ae55be56295f5b2d85c8b19ca0
Ben Carter <benjohn.carter@gmail.com>
2021-10-09 01:53:11 +0100

    refactor: remove old code

--- components/Icon/index.jsx
+++
@@ -1,9 +0,0 @@
-const Icon = ({ icon, className }) => {
-  return (
-    <span className={`icon is-medium ${className}`}>
-      <i className={`${icon} fa-lg navbar-icon`}></i>
-    </span>
-  );
-};
-
-export default Icon;
--- util/messages.js
+++
@@ -1,8 +0,0 @@
-const messages = [
-  "I use coding and algorithms to turn bugs into money",
-  "I write code and sometimes it works",
-];
-
-const lastMessage = "I've run out of funny things to say";
-
-export const messageList = [...messages, lastMessage];
