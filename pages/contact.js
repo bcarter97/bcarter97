7116983b9fc46407b9262ba7bb1314ed32f34384
Ben Carter <benjohn.carter@gmail.com>
2021-10-09 01:57:05 +0100

    feat: add contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,11 +1,26 @@
-import Layout from "components/Layout";
+import { encodeForm } from "common";
+import { Button, Column, Layout } from "components";
 import { Formik } from "formik";
 import Link from "next/link";
 import { useState } from "react";
-import { encodeForm } from "util/common";
 import * as Yup from "yup";
 
-const ContactForm = ({ setSubmit }) => {
+const SubmitMessage = () => (
+  <>
+    <p className="text-xl">
+      Thanks! I&apos;ll get back to you as soon as I can.
+    </p>
+    <Link href="/">
+      <a>
+        <p className="text-xl text-green-500 hover:text-green-800 dark:text-green-400 dark:hover:text-green-100 text-center color-fade">
+          &larr; Back to home
+        </p>
+      </a>
+    </Link>
+  </>
+);
+
+const ContactForm = ({ submit }) => {
   const [submitError, setSubmitError] = useState();
 
   const validationSchema = Yup.object().shape({
@@ -21,7 +36,7 @@ const ContactForm = ({ setSubmit }) => {
       body: encodeForm({ "form-name": "contact", ...values }),
     })
       .then(() => {
-        setSubmit();
+        submit();
         actions.resetForm();
       })
       .catch(() => {
@@ -47,105 +62,109 @@ const ContactForm = ({ setSubmit }) => {
       }) => {
         return (
           <form
+            className="w-full mb-4"
             onSubmit={handleSubmit}
             name="contact"
             data-netlify="true"
             data-netlify-honeypot="bot-field"
           >
             <input type="hidden" name="form-name" value="contact" />
-            <div className="field">
-              <h1 className="title is-size-2">Get in touch.</h1>
+            <h1 className="text text-xl mb-2">Get in touch.</h1>
+
+            <div className="flex flex-wrap -mx-3 mb-2">
+              <div className="w-full md:w-1/2 px-3 mb-2 md:mb-0">
+                <label
+                  className="text block tracking-wide text-gray-700 text-xs font-bold mb-1"
+                  htmlFor="name"
+                >
+                  Name
+                </label>
+                <input
+                  id="name"
+                  name="name"
+                  placeholder="Leonard"
+                  type="text"
+                  className={`
+                    "block w-full text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none outline-none" ${
+                      errors.name && touched.name && "border-red-500"
+                    }`}
+                  value={values.name}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.name && touched.name && (
+                  <p className="text-red-500 dark:text-red-500 text-xs italic -mt-2">
+                    {errors.name}
+                  </p>
+                )}
+              </div>
+              <div className="w-full md:w-1/2 px-3 mb-2 md:mb-0">
+                <label
+                  className="text block tracking-wide text-gray-700 text-xs font-bold mb-1"
+                  htmlFor="email"
+                >
+                  Email
+                </label>
+                <input
+                  id="email"
+                  name="email"
+                  placeholder="lmccoy@enterprise.com"
+                  type="email"
+                  className={`
+                    "block w-full text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none outline-none" ${
+                      errors.email && touched.email && "border-red-500"
+                    }`}
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.email && touched.email && (
+                  <p className="text-red-500 dark:text-red-500 text-xs italic -mt-2">
+                    {errors.email}
+                  </p>
+                )}
+              </div>
+            </div>
+            <div className="flex flex-wrap -mx-3">
+              <div className="w-full px-3">
+                <label
+                  className="text block tracking-wide text-gray-700 text-xs font-bold mb-1"
+                  htmlFor="message"
+                >
+                  Message
+                </label>
+                <textarea
+                  id="message"
+                  name="message"
+                  placeholder="Damn it Jim, I'm a doctor not a form field."
+                  type="text"
+                  className={`
+                    "form-textarea block resize-none w-full text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none outline-none" ${
+                      errors.message && touched.message && "border-red-500"
+                    }`}
+                  value={values.message}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {errors.message && touched.message && (
+                  <p className="text-red-500 dark:text-red-500 text-xs italic -mt-2">
+                    {errors.message}
+                  </p>
+                )}
+              </div>
             </div>
 
-            <div className="columns is-multiline">
-              <div className="column is-6">
-                <div className="field contact-field-name">
-                  <label className="label" htmlFor="name">
-                    Name
-                  </label>
-                  <div className="control">
-                    <input
-                      id="name"
-                      name="name"
-                      placeholder="Leonard"
-                      type="text"
-                      className={`input ${
-                        errors.name && touched.name && "is-danger"
-                      }`}
-                      value={values.name}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                    />
-                    {errors.name && touched.name && (
-                      <p className="help is-danger">{errors.name}</p>
-                    )}
-                  </div>
-                </div>
-              </div>
-              <div className="column is-6">
-                <div className="field contact-field-email">
-                  <label className="label" htmlFor="email">
-                    Email
-                  </label>
-                  <div className="control">
-                    <input
-                      id="email"
-                      name="email"
-                      placeholder="lmccoy@enterprise.com"
-                      type="email"
-                      className={`input ${
-                        errors.email && touched.email && "is-danger"
-                      }`}
-                      value={values.email}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                    />
-                    {errors.email && touched.email && (
-                      <p className="help is-danger">{errors.email}</p>
-                    )}
-                  </div>
-                </div>
-              </div>
-              <div className="column is-12">
-                <div className="field contact-field-message">
-                  <label className="label" htmlFor="message">
-                    Message
-                  </label>
-                  <div className="control">
-                    <textarea
-                      id="message"
-                      name="message"
-                      placeholder="Damn it Jim, I'm a doctor not a form field."
-                      type="text"
-                      className={`textarea has-fixed-size ${
-                        errors.message && touched.message && "is-danger"
-                      }`}
-                      value={values.message}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                    />
-                    {errors.message && touched.message && (
-                      <p className="help is-danger">{errors.message}</p>
-                    )}
-                  </div>
-                </div>
-              </div>
-              <div className="column is-12">
-                <div className="field contact-field-submit">
-                  <div className="control">
-                    <button
-                      type="submit"
-                      className={`button is-primary is-fullwidth ${
-                        isSubmitting ? "is-loading" : ""
-                      }`}
-                    >
-                      Submit
-                    </button>
-                  </div>
-                </div>
-              </div>
+            <div className="mt-2">
+              <Button type="submit" disabled={isSubmitting}>
+                Submit
+              </Button>
             </div>
-            {submitError && <p className="has-text-danger">{submitError}</p>}
+
+            {submitError && (
+              <p className="text-red-500 dark:text-red-500 text-xs italic mt-5">
+                {submitError}
+              </p>
+            )}
           </form>
         );
       }}
@@ -153,28 +172,16 @@ const ContactForm = ({ setSubmit }) => {
   );
 };
 
-const SubmitMessage = () => {
-  return (
-    <>
-      <p className="is-size-4 has-text-centered">
-        Thanks! I&apos;ll get back to you as soon as I can.
-      </p>
-      <p className="is-size-4 has-text-centered">
-        <Link href="/" className="has-text-weight-semibold nav-link">
-          <a>Home</a>
-        </Link>
-      </p>
-    </>
-  );
-};
-
 const Contact = () => {
   const [submitted, setSubmitted] = useState(false);
+
   const submit = () => setSubmitted(true);
 
   return (
     <Layout title="Contact">
-      {submitted ? <SubmitMessage /> : <ContactForm setSubmit={submit} />}
+      <Column>
+        {submitted ? <SubmitMessage /> : <ContactForm submit={submit} />}
+      </Column>
     </Layout>
   );
 };
