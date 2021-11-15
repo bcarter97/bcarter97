c72405150b2c7623d06c778ff455464d842319fb
Ben Carter <benjohn.carter@gmail.com>
2021-11-15 20:29:41 +0000

    feat: blur icon on load (#261)

--- pages/contact.js
+++ pages/contact.js
@@ -67,79 +67,77 @@ const ContactForm = ({ setSubmitted }) => {
         handleChange,
         handleBlur,
         handleSubmit,
-      }) => {
-        return (
-          <form
-            className="w-full mb-4"
-            onSubmit={handleSubmit}
-            name="contact"
-            data-netlify="true"
-            data-netlify-honeypot="bot-field"
-          >
-            <input type="hidden" name="form-name" value="contact" />
-            <h1 className="text text-4xl font-semibold mb-4">Get in touch.</h1>
+      }) => (
+        <form
+          className="w-full mb-4"
+          onSubmit={handleSubmit}
+          name="contact"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
+          <input type="hidden" name="form-name" value="contact" />
+          <h1 className="text text-4xl font-semibold mb-4">Get in touch.</h1>
 
-            <div className="flex flex-wrap -mx-3 mb-2">
-              <div className="w-full md:w-1/2 px-3 mb-2 md:mb-0">
-                <Label htmlFor="name">Name</Label>
-                <Input
-                  id="name"
-                  placeholder="Leonard"
-                  value={values.name}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  errorCondition={errors.name && touched.name}
-                />
-                {errors.name && touched.name && (
-                  <ErrorMessage error={errors.name} />
-                )}
-              </div>
-              <div className="w-full md:w-1/2 px-3 mb-2 md:mb-0">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  placeholder="lmccoy@enterprise.com"
-                  value={values.email}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  errorCondition={errors.email && touched.email}
-                />
-                {errors.email && touched.email && (
-                  <ErrorMessage error={errors.email} />
-                )}
-              </div>
+          <div className="flex flex-wrap -mx-3">
+            <div className="w-full md:w-1/2 px-3 mb-2 md:mb-0">
+              <Label htmlFor="name">Name</Label>
+              <Input
+                id="name"
+                placeholder="Leonard"
+                value={values.name}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                errorCondition={errors.name && touched.name}
+              />
+              {errors.name && touched.name && (
+                <ErrorMessage error={errors.name} />
+              )}
             </div>
-            <div className="flex flex-wrap -mx-3">
-              <div className="w-full px-3">
-                <Label htmlFor="message">Message</Label>
-                <TextArea
-                  id="message"
-                  value={values.message}
-                  placeholder="Damn it Jim, I'm a doctor not a form field."
-                  errorCondition={errors.message && touched.message}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                />
-                {errors.message && touched.message && (
-                  <ErrorMessage error={errors.message} />
-                )}
-              </div>
+            <div className="w-full md:w-1/2 px-3 mb-2 md:mb-0">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                placeholder="lmccoy@enterprise.com"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                errorCondition={errors.email && touched.email}
+              />
+              {errors.email && touched.email && (
+                <ErrorMessage error={errors.email} />
+              )}
             </div>
+          </div>
+          <div className="flex flex-wrap -mx-3">
+            <div className="w-full px-3">
+              <Label htmlFor="message">Message</Label>
+              <TextArea
+                id="message"
+                value={values.message}
+                placeholder="Damn it Jim, I'm a doctor not a form field."
+                errorCondition={errors.message && touched.message}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              {errors.message && touched.message && (
+                <ErrorMessage error={errors.message} />
+              )}
+            </div>
+          </div>
 
-            <div className="mt-6">
-              <Button type="submit" disabled={isSubmitting}>
-                Submit
-              </Button>
-            </div>
+          <div className="mt-6">
+            <Button type="submit" disabled={isSubmitting}>
+              Submit
+            </Button>
+          </div>
 
-            {submitError && (
-              <p className="text-red-500 dark:text-red-500 text-center italic mt-5">
-                {submitError}
-              </p>
-            )}
-          </form>
-        );
-      }}
+          {submitError && (
+            <p className="text-red-500 dark:text-red-500 text-center italic mt-5">
+              {submitError}
+            </p>
+          )}
+        </form>
+      )}
     </Formik>
   );
 };
