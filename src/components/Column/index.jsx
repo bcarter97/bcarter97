27ef4ce70a6b4e4e3b576ec8bb981cf91bdd3c78
Ben Carter <benjohn.carter@gmail.com>
2021-11-22 23:17:33 +0000

    refactor: revert column reducer

--- src/components/Column/index.jsx
+++ src/components/Column/index.jsx
@@ -1,37 +1,11 @@
-const columns = [
-  {
-    size: "md",
-    start: 2,
-    span: 10,
-  },
-  {
-    size: "lg",
-    start: 4,
-    span: 6,
-  },
-  {
-    size: "xl",
-    start: 5,
-    span: 4,
-  },
-];
-
-const Column = ({ children }) => {
-  const responsiveCols = columns.reduce(
-    (acc, { size, start, span }) =>
-      `${acc} ${size}:col-start-${start} ${size}:col-span-${span}`.trim(),
-    ""
-  );
-
-  return (
-    <div className="grid grid-cols-12 p-6 md:py-12 animate-bounce-up">
-      <div
-        className={`col-start-1 col-span-12 ${responsiveCols} bg-gray-50 dark:bg-gray-800 p-6 rounded-xl shadow-lg`}
-      >
-        {children}
-      </div>
+const Column = ({ children }) => (
+  <div className="grid grid-cols-12 p-6 md:py-12 animate-bounce-up">
+    <div
+      className={`col-start-1 col-span-12 md:col-start-2 md:col-span-10 lg:col-start-4 lg:col-span-6 xl:col-start-5 xl:col-span-4 bg-gray-50 dark:bg-gray-800 p-6 rounded-xl shadow-lg`}
+    >
+      {children}
     </div>
-  );
-};
+  </div>
+);
 
 export default Column;
