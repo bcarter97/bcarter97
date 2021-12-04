4156538bbd1ba92f4120981f6a40350c9136e5ab
Ben Carter <benjohn.carter@gmail.com>
2021-12-04 14:24:57 +0000

    refactor: style 404 same as contact

--- pages/404.tsx
+++ pages/404.tsx
@@ -8,17 +8,17 @@ const NotFound: NextPage = () => (
       <p className="text-4xl font-semibold mt-6 mb-4 text-center">
         404... that&apos;s an error.
       </p>
-      <p className="text-2xl font-semibold">
+      <p className="text-2xl font-semibold mb-12">
         This website literally has two pages, so you&apos;re probably here by
         accident.
       </p>
-      <p className="text-2xl font-semibold text-right pt-6">
-        <span>
-          <Link href="/">
-            <a className="hover:opacity-60">Home &rarr;</a>
-          </Link>
-        </span>
-      </p>
+      <Link href="/">
+        <a>
+          <p className="text-3xl text-green-500 hover:text-green-800 dark:text-green-400 dark:hover:text-green-100 text-center color-fade">
+            &larr; Back to home
+          </p>
+        </a>
+      </Link>
     </Column>
   </Layout>
 );
