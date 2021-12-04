dc4f4d6b2a0f6695c1ecdaef746e13d70c1c4385
Ben Carter <benjohn.carter@gmail.com>
2021-12-04 14:20:29 +0000

    refactor: rewrite ErrorMessage

--- src/components/ErrorMessage/index.tsx
+++ src/components/ErrorMessage/index.tsx
@@ -1,4 +1,4 @@
-const ErrorMessage = ({ error }) => (
+const ErrorMessage = ({ error }: { error: string }) => (
   <p className="text-red-500 dark:text-red-500 text-xs italic -mt-2">{error}</p>
 );
 
