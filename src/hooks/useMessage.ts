95c6c0bfe420200f10ca01b347dd6d9dcd9c9077
Ben Carter <benjohn.carter@gmail.com>
2022-01-20 22:45:04 +0000

    refactor: move components to root (#303)

--- src/hooks/useMessage.ts
+++ src/hooks/useMessage.ts
@@ -5,9 +5,8 @@ const useMessage = () => {
   const [messageCount, setMessageCount] = useState(0);
   const message = messages[messageCount];
 
-  const incrementMessage = () => {
+  const incrementMessage = () =>
     setMessageCount((curr) => (curr + 1) % messages.length);
-  };
 
   return [message, incrementMessage] as const;
 };
